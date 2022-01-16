a307ec69b3e39127615a08ed4cc35f48e024e41f
julie-andre <57328172+julie-andre@users.noreply.github.com>
2022-01-16 11:46:35 +0100

    Add choice parameter Query 5

--- Query5/__init__.py
+++ Query5/__init__.py
@@ -11,6 +11,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
+    choice = req.params.get('choice')
+    flag = True
     if not name:
         try:
             req_body = req.get_json()
@@ -18,6 +20,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
+            
+    if not choice or choice.lower() not in ["group","actor","director"]:
+        choice = "group"
+        flag = False
     
     server = os.environ["TPBDD_SERVER"]
     database = os.environ["TPBDD_DB"]
@@ -39,37 +45,50 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
                 cursor = conn.cursor()
                 
-                # First SQL Query 
-                cursor.execute("SELECT genre, SUM(runtimeMinutes)/COUNT(*) AS avg_time FROM [dbo].[tTitles] as T, [dbo].[tGenres] as G WHERE T.tconst = G.tconst GROUP BY genre ORDER BY avg_time")
+                # On hardcode 3 requêtes non paramétrées afin d'éviter les injections SQL
+                # Remarque : on pourrait aussi utiliser un framework de préparateur de requêtes (puisque les 2 dernières requêtes se ressemblent)
+                # Mais ici le gain en lignes de codes en utulisant cette méthode serait négligeable
+                if(choice == "group"):
+                    # First SQL Query 
+                    cursor.execute("SELECT genre, SUM(runtimeMinutes)/COUNT(*) AS avg_time FROM [dbo].[tTitles] as T, [dbo].[tGenres] as G WHERE T.tconst = G.tconst GROUP BY genre ORDER BY avg_time")
 
-                rows = cursor.fetchall()
-                for row in rows:
-                    dataString += f"SQL: Genre={row[0]}, Average Time={row[1]}\n"
+                    rows = cursor.fetchall()
+                    for row in rows:
+                        dataString += f"SQL: Genre={row[0]}, Average Time={row[1]}\n"
                     
-                #Second SQL Query
-                dataString += "\n\n\nDurée moyenne des films dans lesquels chaque acteur a joué\n\n"
-                cursor.execute("SELECT primaryName, SUM(runtimeMinutes)/COUNT(DISTINCT t.tconst) AS avg_time FROM [dbo].[tTitles] as T, [dbo].[tPrincipals] as P, [dbo].[tNames] as N WHERE T.tconst = P.tconst AND P.nconst = N.nconst AND P.category = 'acted in' GROUP BY primaryName ORDER BY avg_time")
+                elif (choice == "actor"):
+                    #Second SQL Query
+                    dataString += "Durée moyenne des films dans lesquels chaque acteur a joué\n\n"
+                    cursor.execute("SELECT primaryName, SUM(runtimeMinutes)/COUNT(DISTINCT t.tconst) AS avg_time FROM [dbo].[tTitles] as T, [dbo].[tPrincipals] as P, [dbo].[tNames] as N WHERE T.tconst = P.tconst AND P.nconst = N.nconst AND P.category = 'acted in' GROUP BY primaryName ORDER BY avg_time")
 
-                rows = cursor.fetchall()
-                for row in rows:
-                    dataString += f"SQL: Primary Name={row[0]}, Average Time={row[1]}\n"
+                    rows = cursor.fetchall()
+                    for row in rows:
+                        dataString += f"SQL: Primary Name={row[0]}, Average Time={row[1]}\n"
                 
-                #Third SQL Query
-                dataString += "\n\n\nDurée moyenne des films que chaque directeur a dirigé\n\n"
-                cursor.execute("SELECT primaryName, SUM(runtimeMinutes)/COUNT(DISTINCT t.tconst) AS avg_time FROM [dbo].[tTitles] as T, [dbo].[tPrincipals] as P, [dbo].[tNames] as N WHERE T.tconst = P.tconst AND P.nconst = N.nconst AND P.category = 'directed' GROUP BY primaryName ORDER BY avg_time")
+                elif (choice == "director"):
+                    #Third SQL Query
+                    dataString += "Durée moyenne des films que chaque directeur a dirigé\n\n"
+                    cursor.execute("SELECT primaryName, SUM(runtimeMinutes)/COUNT(DISTINCT t.tconst) AS avg_time FROM [dbo].[tTitles] as T, [dbo].[tPrincipals] as P, [dbo].[tNames] as N WHERE T.tconst = P.tconst AND P.nconst = N.nconst AND P.category = 'directed' GROUP BY primaryName ORDER BY avg_time")
 
-                rows = cursor.fetchall()
-                for row in rows:
-                    dataString += f"SQL: Primary Name={row[0]}, Average Time={row[1]}\n"
+                    rows = cursor.fetchall()
+                    for row in rows:
+                        dataString += f"SQL: Primary Name={row[0]}, Average Time={row[1]}\n"
 
     except:
         errorMessage = "Erreur de connexion a la base SQL"
         
     
-    if name:
-        nameMessage = f"Hello, {name}!\n"
+    if (!flag):
+        nameMessage = "\nAucune critère n'a été passé en paramètre, ou alors un critère invalide a été passé en paramètre\n"
+        nameMessage += "Le parametre 'group' a été considéré par défaut pour cette requête\n"
     else:
-        nameMessage = "Le parametre 'name' n'a pas ete fourni lors de l'appel.\n"
+        nameMessage += f"Les résultats ont été obtenus avec le critère {choice}\n"
+    
+    nameMessage += "Si vous souhaitez avoir les résultats de la requête selon : \n - le critère groupe, ajoutez '&choice=group' à l'url\n- le critère acteur, ajoutez '&choice=actor' à l'url\n - le critère directeur, ajoutez '&choice=director' à l'url\n"
+    if name:
+        nameMessage += f"Hello, {name}!\n"
+    else:
+        nameMessage += "Le parametre 'name' n'a pas ete fourni lors de l'appel.\n"
     
     if errorMessage != "":
         return func.HttpResponse(dataString + nameMessage + errorMessage, status_code=500)
